3cf212e20fad081450eaa9123a18ac52fa686adf
mxdi9i7 <peter.zheng88228@gmail.com>
2020-05-22 16:31:19 -0400

    made it work

---
+++ demo/index.js
@@ -0,0 +1,53 @@
+// @collapse
+import foo from 'foo';
+import bar from 'bar';
+import foobar from 'foobar';
+
+const myFunction = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction2 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction3 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction4 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction5 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction6 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction7 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction8 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction9 = () => {
+	const foo = 'bar';
+	return foo;
+};
+const myFunction10 = () => {
+	const foo = 'bar';
+	return foo;
+};
+
+const exporter = {
+	myFunction,
+	myFunction2,
+	myFunction3,
+};
+
+export default exporter;
--- extension.js
+++ extension.js
@@ -1,37 +1,54 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/**
+ * Folds regions of default level and all their inner regions up to level 7.
+ * @param resourceUri
+ */
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "auto-collapse-blocks" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-collapse-blocks.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from auto-collapse-blocks!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'auto-collapse-blocks.activate',
+			foldAllCodeBlocks,
+		),
+		vscode.window.onDidChangeActiveTextEditor((editor) => {
+			if (editor) {
+				foldAllCodeBlocks();
+			}
+		}),
+		vscode.workspace.onDidSaveTextDocument((editor) => {
+			if (editor) {
+				foldAllCodeBlocks();
+			}
+		}),
+	);
 }
+
+function hasCollapsePragma(content) {
+	return /^\s*(\/*\*+|\/\/)\s*@collapse/m.test(content);
+}
+
+function foldAllCodeBlocks() {
+	if (
+		vscode.window.activeTextEditor &&
+		hasCollapsePragma(vscode.window.activeTextEditor.document.getText())
+	) {
+		for (let i = 0; i <= 7; i++) {
+			vscode.commands.executeCommand(`editor.foldLevel${i}`);
+		}
+	} else {
+		vscode.commands.executeCommand('editor.unfoldAll');
+	}
+}
+
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
 	activate,
-	deactivate
-}
+	deactivate,
+};
